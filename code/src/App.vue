d7f77b5e1387ad6859c95a468d15c36a4edc11b6
jiangrui618 <842688489@qq.com>
2020-11-11 21:32:24 +0800

    修改项目

--- code/src/App.vue
+++ code/src/App.vue
@@ -1,9 +1,23 @@
 <template>
   <div id="app">
-    <h1>测试vue项目</h1>
+    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
-<style lang="less">
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- code/src/router/index.js
+++ code/src/router/index.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = []
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
 })
-
-export default router
